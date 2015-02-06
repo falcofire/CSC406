cc1aa9cbd31d7a9bee56c04fcb238639509c3254
falcofire <falcofire@users.noreply.github.com>
2015-02-06 16:04:56 -0500

    Create AMDG.java

---
+++ AMDG.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+
+
+public class AMDG extends G{
+
+	private static Edge[][] unweightedMatrix = new Edge[Tester.size][Tester.size];
+	
+	public AMDG (){
+	}
+	
+	public static boolean existEdge(Edge e){
+		Node node1 = e.getVertex1();
+		Node node2 = e.getVertex2();
+		if (unweightedMatrix[node1.getNode()][node2.getNode()] != null)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean existEdge(int i, int j){
+		if (unweightedMatrix[i][j] != null)
+			return true;
+		else
+			return false;
+	}
+	
+	protected void putEdge(Edge edge){
+		if (!existEdge(edge)){
+			Node node1 = edge.getVertex1();
+			Node node2 = edge.getVertex2();
+			unweightedMatrix[node1.getNode()][node2.getNode()] = edge;
+		}
+	}
+	
+	protected static void putEdge(int i, int j){
+		if (!existEdge(i, j)){
+			Node node1 = new Node(i);
+			Node node2 = new Node(j);
+			Edge edge = new Edge(node1, node2);
+			unweightedMatrix[i][j] = edge;
+		}	
+	}
+	
+	public static void removeEdge(Edge edge){
+		Node node1 = edge.getVertex1();
+		Node node2 = edge.getVertex2();
+		unweightedMatrix[node1.getNode()][node2.getNode()] = null;
+	}
+	
+	public static void removeEdge(int i, int j){
+		unweightedMatrix[i][j] = null;
+	}
+	
+	public static ArrayList<Node> adjacentVertices(Node i){
+		ArrayList<Node> adjNodes= new ArrayList<Node>();
+		for (int j = 0; j < unweightedMatrix.length; j++){
+			if (unweightedMatrix[j][i.getNode()] != null)
+				adjNodes.add(unweightedMatrix[j][i.getNode()].getVertex2());
+		}
+		return adjNodes;
+	}
+	
+	public static ArrayList<Node> adjacentVertices(int i){
+		ArrayList<Node> adjNodes = new ArrayList<Node>();
+		for (int j = 0; j < unweightedMatrix.length; j++){
+			if (unweightedMatrix[j][i] != null)
+				adjNodes.add(unweightedMatrix[j][i].getVertex2());
+		}
+		return adjNodes;
+	}
+	
+	public static boolean areAdjacent(Node i, Node j){
+		if (unweightedMatrix[i.getNode()][j.getNode()] != null || unweightedMatrix[j.getNode()][i.getNode()] != null)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean areAdjacent(int i, int j){
+		if (unweightedMatrix[i][j] != null || unweightedMatrix[j][i] != null)
+			return true;
+		else
+			return false;
+	}
+	
+	public static Edge[][] getMatrix(){
+		return unweightedMatrix;
+	}
+
+	protected void initializeList() {
+		
+	}
+
+}

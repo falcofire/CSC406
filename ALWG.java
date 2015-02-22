cc79fa6daa3e84ab9294eac8e6cc21afdfb2f76d
falcofire <ztubb1@brockport.edu>
2015-02-22 12:03:43 -0500

    New class for Assign2

---
+++ ALWG.java
@@ -0,0 +1,60 @@
+import java.util.List;
+
+
+public class ALWG extends G{
+
+	@Override
+	protected void initializeList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean areAdjacent(int i, int j) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected List<Integer> adjacentVertices(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean existEdge(int i, int j) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void removeEdge(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void putEdge(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void putEdge(int i, int j, int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void putEdge(Edge e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

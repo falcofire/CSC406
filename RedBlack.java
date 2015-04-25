3d84736eae3dc02946f0f90460bed2395c08f87b
falcofire <ztubb1@brockport.edu>
2015-04-25 13:19:01 -0400

    Contructed basic framework for RedBlack Tree class.

---
+++ RedBlack.java
@@ -0,0 +1,26 @@
+
+public class RedBlack {
+
+	protected void put(int n){
+		
+	}
+	
+	protected Node find(int n){
+		
+		
+		return null;
+	}
+	
+	protected void insert(Node n){
+		
+	}
+	
+	protected void remove(int n){
+		
+	}
+	
+	protected void printTree(Node n){
+		
+	}
+	
+}
